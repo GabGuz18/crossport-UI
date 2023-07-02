0341feb26535d843e4f66ea01c402d5ee353f94d
Diego Guzman <diegog.guzman@outlook.com>
2023-07-02 12:43:18 -0600

    Nuevo proyecto

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api', 
-        destination: 'http://127.0.0.1:8000/'
-      }
-    ]
-  }
-};
+const nextConfig = {}
+
+module.exports = nextConfig
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,9 @@
+import { Login } from '@/components/Login'
 import React from 'react'
-import Register from '@/public/components/Register'
 
 const Page = () => {
   return (
-    <Register />
+    <Login/>
   )
 }
 
