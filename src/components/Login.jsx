3cb3aef844dba5a233397294ba6e34954d688dd1
LuisQuintero <luisgqr1975@gmail.com>
2023-07-05 14:23:52 -0600

    class a className y creación de folder de style

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,46 +2,46 @@ import React from 'react'
 
 export const Login = () => {
   return (
-    <div class="limiter">
-        <div class="container-login100">
-          <div class="wrap-login100 p-t-50 p-b-90">
+    <div className="limiter">
+        <div className="container-login100">
+          <div className="wrap-login100 p-t-50 p-b-90">
             {/* Form Login*/}
-            <form class="login100-form validate-form flex-sb flex-w">
-              <span class="login100-form-title p-b-51">Login</span>
+            <form className="login100-form validate-form flex-sb flex-w">
+              <span className="login100-form-title p-b-51">Login</span>
               {/* Falta lo de validate   Login Username*/}
-              <div class="wrap-input100 validate-input m-b-16">
-                <input class="input100" type="text" name="username" placeholder="Username"/>
-                <span class="focus-input100"></span>
+              <div className="wrap-input100 validate-input m-b-16">
+                <input className="input100" type="text" name="username" placeholder="Username"/>
+                <span className="focus-input100"></span>
               </div>
               {/* Falta lo de validate    Contraseña*/}
-              <div class="wrap-input100 valite-input m-b-16">
-                <input class="input100" type="password" name="pass" placeholder="Password"/>
-                <span class="focus-input100"></span>
+              <div className="wrap-input100 valite-input m-b-16">
+                <input className="input100" type="password" name="pass" placeholder="Password"/>
+                <span className="focus-input100"></span>
               </div>
               {/* Remember me  Forgot password*/}
-              <div class="flex-sb-m w-full p-t-3 p-b-24">
+              <div className="flex-sb-m w-full p-t-3 p-b-24">
                 {/* Remember me  */}
-                <div class="contact100-form-checkbox">
+                <div className="contact100-form-checkbox">
                   {/* falta un input id=""*/}
-                  <label class="label-checkbox" htmlFor="ckb1">
+                  <label className="label-checkbox" htmlFor="ckb1">
                     Recuerdame
                   </label>
                 </div>
                 {/* Forgot password*/}
                 <div>
-                  <a class="txt1" href="#">¿Olvidaste tu contraseña?</a>
+                  <a className="txt1" href="#">¿Olvidaste tu contraseña?</a>
                 </div>
               </div>
             {/* Login Button*/}
-            <div class="container-login100-form-btn m-t-17">
-              <button class="login100-form-btn">
+            <div className="container-login100-form-btn m-t-17">
+              <button className="login100-form-btn">
                 Login
               </button>
             </div>
 
             {/* Register Button*/}
-            <div class="container-login100-form-btn m-t-17">
-              <button class="login100-form-btn">
+            <div className="container-login100-form-btn m-t-17">
+              <button className="login100-form-btn">
                 Registrar
               </button>
             </div>
