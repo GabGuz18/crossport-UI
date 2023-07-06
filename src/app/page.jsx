646d10e4b1b8d6b07c9e02aa38f777b005a2e74a
Gabo Guzman <57119079+GabGuz18@users.noreply.github.com>
2023-07-06 11:09:34 -0600

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,13 @@
+import Link from "next/link"
 
 const Home = () => {
   return (
     <div>
+      <h1>Hola</h1>
       <Link href={"/Login"}>
-      Login1
+        Login1
       </Link>
     </div>
-    
   )
 }
 
